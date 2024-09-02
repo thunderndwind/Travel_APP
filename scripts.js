7b0d385a7204c9cc3ac2050735a60e65a2195350
thunderndwind <bmaher.ca@gmail.com>
2024-09-02 12:26:43 +0300

    init version

---
+++ scripts.js
@@ -0,0 +1,57 @@
+async function fetchData() {
+    try {
+        console.log("trying");
+        const response = await fetch('data.json');
+        console.log("fetched");
+        return await response.json();
+
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return null;
+    }
+}
+
+async function handleSearch(event) {
+    event.preventDefault();
+
+    const data = await fetchData();
+    if (!data) {
+        document.getElementById('result').innerText = 'No results found';
+        return;
+    }
+
+    const searchQuery = document.getElementById('destinationInput').value.toLowerCase();
+    let results = '';
+
+    if (searchQuery == 'temples' || searchQuery == 'temple') {
+        data.temples.forEach(temple => {
+            results += `<div><img src="images/${temple.imageUrl}" alt="${temple.name}"><h3>${temple.name}</h3><p>${temple.description}</p></div>`;
+
+        });
+    } else if (searchQuery == 'beaches' || searchQuery == 'beach') {
+        data.beaches.forEach(beach => {
+            results += `<div><img src="images/${beach.imageUrl}" alt="${beach.name}"><h3>${beach.name}</h3><p>${beach.description}</p></div>`;
+        });
+    } else {
+
+        data.countries.forEach(country => {
+            if (country.name.toLowerCase().includes(searchQuery)) {
+                country.cities.forEach(city => {
+                    results += `<img src="images/${city.imageUrl}" alt="${city.name}"><div><h3>${city.name}</h3><p>${city.description}</p></div>`;
+                });
+            }
+        });
+    }
+    document.querySelector('#result').classList.add("result-card");
+    document.getElementById('result').innerHTML = results;
+}
+
+function clearResults() {
+    document.getElementById('destinationInput').value = '';
+    document.getElementById('result').innerHTML = '';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('btnSearch').addEventListener('click', handleSearch);
+    document.getElementById('btnClear').addEventListener('click', clearResults);
+});
